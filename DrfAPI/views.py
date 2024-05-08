6edaf6cae8a58f49f18ad4ce6d43fa28bf3c0424
Hassan Arjmandnia <hassanarjmandnia1998@gmail.com>
2024-05-08 07:48:58 +0330

    Add LOG system

--- DrfAPI/views.py
+++ DrfAPI/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import logging
+from django.http import HttpResponse
+from rest_framework.views import APIView
 
-# Create your views here.
+logger = logging.getLogger("DrfAPI")
+
+
+class MyAPIView(APIView):
+    def get(self, request):
+        logger.debug("This is a debug message")
+        logger.info("This is an info message")
+        logger.warning("This is a warning message")
+        logger.error("This is an error message")
+        logger.critical("This is a critical message")
+        return HttpResponse("Hello, world!")
