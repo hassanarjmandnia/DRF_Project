dbf6b017b43a992e0bc72f564fdfe0b86f748964
Hassan Arjmandnia <hassanarjmandnia1998@gmail.com>
2024-05-10 07:28:06 +0330

    update the log system

--- DrfAPI/views.py
+++ DrfAPI/views.py
@@ -1,15 +1,19 @@
-import logging
-from django.http import HttpResponse
 from rest_framework.views import APIView
+from django.http import HttpResponse
+import logging
 
-logger = logging.getLogger("DrfAPI")
+debug_logger = logging.getLogger("debug_logger")
+info_logger = logging.getLogger("info_logger")
+warning_logger = logging.getLogger("warning_logger")
+error_logger = logging.getLogger("error_logger")
+critical_logger = logging.getLogger("critical_logger")
 
 
 class MyAPIView(APIView):
     def get(self, request):
-        logger.debug("This is a debug message")
-        logger.info("This is an info message")
-        logger.warning("This is a warning message")
-        logger.error("This is an error message")
-        logger.critical("This is a critical message")
+        debug_logger.debug("This is a debug message")
+        info_logger.info("This is an info message")
+        warning_logger.warning("This is a warning message")
+        error_logger.error("This is an error message")
+        critical_logger.critical("This is a critical message")
         return HttpResponse("Hello, world!")
