bd62e95c2aa3b722889d9bbdd6d1bb26fd0384cc
Hassan Arjmandnia <hassanarjmandnia1998@gmail.com>
2024-05-30 02:34:31 +0330

    updating the sale table!

--- DrfAPI/models.py
+++ DrfAPI/models.py
@@ -90,4 +90,9 @@ class Sale(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, null=False, blank=False
     )
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False)
+    total_price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, blank=False
+    )
     created_at = models.DateTimeField(auto_now_add=True)
